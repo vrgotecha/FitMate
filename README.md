90fb093f9102c4f2bef6d3bd8205acb3a987ebe8
Vivek Gotecha <vivekrgotecha@gmail.com>
2024-03-14 16:41:50 +0530

    added FitMate files

---
+++ README.md
@@ -0,0 +1,23 @@
+# MERN FitMate
+
+1) Clone the repository : **https://github.com/vrgotecha/FitMate.git**
+
+2) Edit .env file by entering **PORT**, your **MONGO_URI** (Database url) and your **Secret Key** -
+   
+   PORT = ""
+   
+   MONGO_URI = ""
+   
+   SECRET="" 
+
+4) cd backend -
+   
+   a) Install all the dependencies by running the command - **npm install** or **npm i**
+
+   b) run the server.js by the command in a new terminal - npm run dev
+
+6) cd frontend -
+
+   a) Install all the dependencies by running the command - **npm install** or **npm i**
+
+   b) run the frontend file **index.js** built using React by the command in a new terminal - **npm start**
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const workRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
+require('dotenv').config()
+
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workRoutes)
+app.use('/api/user', userRoutes)
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        const port = process.env.PORT || 4000
+        app.listen(port, () => {
+            console.log(`listening on port ${port}!!!`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
