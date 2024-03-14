45ee11a80e0fdb9aa5f38394fa0de5182ea5bfb3
Vivek Gotecha <vivekrgotecha@gmail.com>
2024-03-14 17:20:55 +0530

    changed frontend/Pages/Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts', {
+      const response = await fetch('https://fitmate-riu7.onrender.com/api/workouts', {
         headers: {
           'Authorization': `Bearer ${user.token}`
         }
